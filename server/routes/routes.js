c8f1c38b961eafc3edc53ff2ea2c8d73d621833a
Spencer Snyder <sasnyde2@gmail.com>
2016-12-28 22:02:24 -0500

    added individual todo route

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -6,9 +6,8 @@ which is just a simple "todo" app
 import express from 'express';
 import mongoose from 'mongoose';
 import Todo from '../models/todo';
-
+/******Set up the router******/
 const router = express.Router();
-
 /*****************************
 GET /api returns all todos
 At some point in the future they will
@@ -17,15 +16,36 @@ order of when they were made
 ******************************/
 router.get('/', (req, res, next) => {
   Todo.find({})
-    .then((doc) => {
-      console.log(doc);
-      res.send(doc);
+    .then( (todos) => {
+      console.log(todos);
+      res.send({
+        todos
+      });
     })
-    .catch((err) => {
+    .catch( (err) => {
       return next(err);
     });
 });
-
+/*******************************
+GET /:id will return an individual todo item
+and all its saved details
+********************************/
+router.get('/:id', (req, res, next) => {
+  Todo.findOne({ _id: req.params.id })
+    .then( (todo) => {
+      if(!todo){
+        let err = new Error;
+        err.status = 404,
+        err.message = "Todo item was not found"
+        return next(err);
+      }
+      res.send(todo);
+    })
+    .catch( (err) => {
+      err.status = 400;
+      return next(err);
+    });
+})
 /*****************************
 POST /api adds a todo to Database
 Simple enough ;)
@@ -33,15 +53,14 @@ Simple enough ;)
 router.post('/', (req, res, next) => {
   let todo = new Todo(req.body);
   todo.save()
-    .then((doc) => {
+    .then( (doc) => {
       console.log(doc);
       res.send(doc);
     })
-    .catch((err) => {
+    .catch( (err) => {
       err.status = 400;
       return next(err);
     });
 });
-
-
+/*Export the router as a module*/
 export default router;
