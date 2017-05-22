8cbf9758ef3857d9cce8575663a0f3d93e985860
Spencer Snyder <sasnyde2@gmail.com>
2017-05-21 20:51:58 -0400

    added untested fb router

--- server/routes/users.js
+++ server/routes/users.js
@@ -52,4 +52,57 @@ router.post('/login', (req, res, next) => {
     });
 });
 
+// POST /users/facebook
+// creates or signs in a new user with a fb account
+router.post('/facebook', (req, res, next) => {
+  let { facebook } = req.body;
+  console.log('req.body', req.body);
+  User.findOne({email: facebook.email })
+    .then(user => {
+      // if user exists
+      if (user) {
+        // and if no facebook account
+        if (!user.facebook) {
+          user.facebook = facebook;
+          // update account and save
+          user.save()
+            .then( () => {
+              // after the user is saved, a token is generated
+              return user.generateAuthToken();
+            })
+            .then( token => {
+              // token is passed and set as res header
+              res.header('Access-Control-Expose-Headers', 'x-auth');
+              res.header('x-auth', token).send(user);
+            })
+            .catch( err => {
+              err.status = 400;
+              return next(err);
+            });
+            // else fb is there, just send token for Todos
+        } else {
+          return user.generateAuthToken();
+        }
+        // or if there is no user found
+      } else {
+        //create user from facebook details with no password
+        let user = new User({email: facebook.email, facebook });
+        user.save()
+          .then( () => {
+            // after the user is saved, a token is generated
+            return user.generateAuthToken();
+          })
+          .then( token => {
+            // token is passed and set as res header
+            res.header('Access-Control-Expose-Headers', 'x-auth');
+            res.header('x-auth', token).send(user);
+          })
+          .catch( err => {
+            err.status = 400;
+            return next(err);
+          });
+      }
+    })
+})
+
 export default router;
