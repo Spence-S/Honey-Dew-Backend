572e03b35e1553ff41e8084a7dc959e7a3ec3b59
Spencer Snyder <sasnyde2@gmail.com>
2017-05-28 20:16:03 -0400

    fixed findByCredentials method that needed changing from model change.

--- server/models/user.js
+++ server/models/user.js
@@ -126,7 +126,7 @@ UserSchema.statics.findByToken = function (token) {
 UserSchema.statics.findByCredentials = function (email, password) {
   var User = this;
 
-  return User.findOne({email}).then((user) => {
+  return User.findOne({'email.email': email}).then((user) => {
     if (!user) {
       return Promise.reject();
     }
